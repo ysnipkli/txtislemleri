6c463a820f1c2ea6dd3b3cc7ea19661877019989
ysnipekli <ykipekli@gmail.com>
2021-03-22 17:02:42 +0300

    TxtDüzenlemeProjesi

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace txtislemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_arama_yap = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(297, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(464, 403);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dosya Aç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_arama_yap
+            // 
+            this.btn_arama_yap.Location = new System.Drawing.Point(216, 62);
+            this.btn_arama_yap.Name = "btn_arama_yap";
+            this.btn_arama_yap.Size = new System.Drawing.Size(75, 23);
+            this.btn_arama_yap.TabIndex = 2;
+            this.btn_arama_yap.Text = "Ara";
+            this.btn_arama_yap.UseVisualStyleBackColor = true;
+            this.btn_arama_yap.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Değistir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(216, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Bul";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "F komutu (Ara)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "R komutu(Değiştir)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "D komutu(Sil)";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(216, 210);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(107, 212);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_arama_yap);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_arama_yap;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace txtislemleri
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opn = new OpenFileDialog();
+            opn.Filter = "TXT|*.txt|NC|*.nc"; //sadece txt dosyalarına erişim sağlamak için.
+            if (opn.ShowDialog() == System.Windows.Forms.DialogResult.Cancel) //dosya windows formuna uygunsa iptal et değilse else işlemi ile devam et.
+            {
+                return;
+            }
+            else
+            { //seçtiğimiz dosyanın içeriğini okumak için
+                StreamReader oku = new StreamReader(opn.FileName); //girilen dosya ismini okuyor ve açıyor.
+                richTextBox1.Text = oku.ReadToEnd();
+                oku.Close();
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            int indeks = 0;
+            string t = richTextBox1.Text; //richttextboxtaki bulma ve boyama işlemi yapılacak, o işaretleme devamlı olmasın diye
+            richTextBox1.Text = "";  //işaretlenen ifadeyi clearladık.
+            richTextBox1.Text = t;
+
+            //string txtparameter_1 = @".\-$";
+            // bool istxt1 = Regex.IsMatch(textBox1.Text, txtparameter_1);
+
+            string _String = textBox1.Text;  //bu kısımda girilen kelimede * veya - varmı kntrolü yapmak için kontrol değerleri oluşturduk.
+            string _Kontrol = "-";
+            string _Stringg = textBox1.Text;
+            string _Kontroll = "*";
+
+            
+           
+
+            if ((_String.StartsWith(_Kontrol)) && (_String.EndsWith(_Kontrol)))  //eğer ilk veya son stringi - ise
+            {
+                string sorun = textBox1.Text;
+                string cozum = sorun.Substring(1, sorun.Length - 1).Substring(0, sorun.Length - 2);  //textboxtaki ifadenin başından ve sonundan eksilterek aramaya geçeceğiz.
+                textBox1.Text = cozum;
+                
+
+                int sayac = 0;
+                while (indeks <= richTextBox1.Text.ToLower().LastIndexOf(textBox1.Text))  //son stringe kadar git.
+                {
+                    
+                 
+                    richTextBox1.Find(textBox1.Text, indeks, richTextBox1.TextLength, RichTextBoxFinds.None); //aranan kelimeyi bul
+                    richTextBox1.SelectionBackColor = Color.Yellow; //arka planı sarıya çevirki bulduğun belli olsun
+                    indeks = richTextBox1.Text.ToLower().IndexOf(textBox1.Text, indeks) + 1; //aynı kelimeden fazlası varsa onları da tek tek bulmak için
+                    sayac++; //kac tane olduğunu ekrana yazdırmak için
+
+                }
+                
+
+                MessageBox.Show(sayac + "Adet bulundu");
+            }
+
+            else if ((_Stringg.StartsWith(_Kontroll)) && (_Stringg.EndsWith(_Kontroll)))
+            {
+                string sorun = textBox1.Text;
+                string cozum = sorun.Substring(1, sorun.Length - 1).Substring(0, sorun.Length - 2);
+                textBox1.Text = cozum;
+
+                int sayac = 0;
+                while (indeks <= richTextBox1.Text.ToLower().LastIndexOf(textBox1.Text))
+                {
+
+
+                    richTextBox1.Find(textBox1.Text, indeks, richTextBox1.TextLength, RichTextBoxFinds.None);
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                    indeks = richTextBox1.Text.ToLower().IndexOf(textBox1.Text, indeks) + 1;
+                    sayac++;
+
+                }
+                MessageBox.Show(sayac + "Adet bulundu");
+            }
+
+            else
+            {
+                int sayac = 0;
+                while (indeks <= richTextBox1.Text.ToLower().LastIndexOf(textBox1.Text))
+                {
+
+                    richTextBox1.Find(textBox1.Text, indeks, richTextBox1.TextLength, RichTextBoxFinds.None);
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                    indeks = richTextBox1.Text.ToLower().IndexOf(textBox1.Text, indeks) + 1;
+                    sayac++;
+
+                }
+
+
+                MessageBox.Show(sayac + "Adet bulundu");
+            }
+
+
+        }
+        //bulduğu kelimeleri sarıyla, değiştirdiklerini kırmızı highlight ile belirtecek şekilde kodlandı.
+        private void button3_Click(object sender, EventArgs e) //bul  
+        {
+            
+            string t_1 = richTextBox1.Text;
+            richTextBox1.Text = "";
+            richTextBox1.Text = t_1;
+            string _String_1 = textBox3.Text;
+            string _Kontrol_1 = "-";
+            string _Stringg_1 = textBox3.Text;
+            string _Kontroll_1 = "*";
+            int indeks_1 = 0;
+
+
+            if ((_String_1.StartsWith(_Kontrol_1)) && (_String_1.EndsWith(_Kontrol_1)))
+            {
+                string sorun = textBox3.Text;
+                string cozum = sorun.Substring(1, sorun.Length - 1).Substring(0, sorun.Length - 2);
+                textBox3.Text = cozum;
+
+
+                
+                while (indeks_1 <= richTextBox1.Text.ToLower().LastIndexOf(textBox3.Text))
+                {
+                    richTextBox1.Find(textBox3.Text, indeks_1, richTextBox1.TextLength, RichTextBoxFinds.None);
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                    indeks_1 = richTextBox1.Text.ToLower().IndexOf(textBox3.Text, indeks_1) + 1;
+                    //richTextBox1.Find(textBox3.Text, 0, RichTextBoxFinds.None); //aranan kelimeyi bulma işlemi bitene kadar ara.
+                    //richTextBox1.Focus(); //yazılan kelimeyi belli etmesi için
+                }
+
+            }
+
+            else if ((_Stringg_1.StartsWith(_Kontroll_1)) && (_Stringg_1.EndsWith(_Kontroll_1)))
+            {
+                
+                string sorun = textBox3.Text;
+                string cozum = sorun.Substring(1, sorun.Length - 1).Substring(0, sorun.Length - 2); 
+                textBox3.Text = cozum;
+
+                
+                while (indeks_1 <= richTextBox1.Text.ToLower().LastIndexOf(textBox3.Text))
+                {
+
+
+                    richTextBox1.Find(textBox3.Text, indeks_1, richTextBox1.TextLength, RichTextBoxFinds.None);
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                    indeks_1 = richTextBox1.Text.ToLower().IndexOf(textBox3.Text, indeks_1) + 1;
+                    
+
+                }
+                
+            }
+
+            else
+            {
+                
+                while (indeks_1 <= richTextBox1.Text.ToLower().LastIndexOf(textBox3.Text))
+                {
+
+                    richTextBox1.Find(textBox3.Text, indeks_1, richTextBox1.TextLength, RichTextBoxFinds.None);
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                    indeks_1 = richTextBox1.Text.ToLower().IndexOf(textBox3.Text, indeks_1) + 1;
+                    
+
+                }
+            }
+
+            
+
+        }
+        private void button2_Click_1(object sender, EventArgs e) //degistir
+        {
+            int temp = 0;
+            while(temp<= richTextBox1.Text.ToLower().LastIndexOf(textBox3.Text)) //aradığımız son kelimeyi textin sonuna kadar arıcak ilk kelimeye kadar tarıcak.
+            {
+                richTextBox1.Find(textBox3.Text, temp, richTextBox1.TextLength, RichTextBoxFinds.None);
+                richTextBox1.SelectionBackColor = Color.Red;
+                temp = richTextBox1.Text.ToLower().IndexOf(textBox3.Text, temp) + 1;
+                richTextBox1.SelectedText = textBox2.Text; // yazılan kelimeyi texte tek tıkla istenilen hale çevirecek. //değistri kısmında yazılan ifadeyi değiştirmesi için ata.
+            }
+
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            string t_2 = richTextBox1.Text;
+            richTextBox1.Text = "";
+            richTextBox1.Text = t_2;
+            string _String_2 = textBox4.Text;
+            string _Kontrol_2 = "-";
+            string _Stringg_2 = textBox4.Text;
+            string _Kontroll_2 = "*";
+            int indeks_2 = 0;
+
+            
+            {
+                if ((_String_2.StartsWith(_Kontrol_2)) && (_String_2.EndsWith(_Kontrol_2)))
+                {
+                    string sorun = textBox4.Text;
+                    string cozum = sorun.Substring(1, sorun.Length - 1).Substring(0, sorun.Length - 2);
+                    textBox4.Text = cozum;
+
+
+
+                    while (indeks_2 <= richTextBox1.Text.ToLower().LastIndexOf(textBox4.Text))
+                    {
+                        richTextBox1.Find(textBox4.Text, indeks_2, richTextBox1.TextLength, RichTextBoxFinds.None);
+                        indeks_2 = richTextBox1.Text.ToLower().IndexOf(textBox4.Text, indeks_2) + 1;
+                        richTextBox1.SelectionBackColor = Color.White;
+                        richTextBox1.SelectedText = " ";
+                    }
+
+                }
+
+                else if ((_Stringg_2.StartsWith(_Kontroll_2)) && (_Stringg_2.EndsWith(_Kontroll_2)))
+                {
+
+                    string sorun = textBox4.Text;
+                    string cozum = sorun.Substring(1, sorun.Length - 1).Substring(0, sorun.Length - 2);
+                    textBox4.Text = cozum;
+
+
+                    while (indeks_2 <= richTextBox1.Text.ToLower().LastIndexOf(textBox4.Text))
+                    {
+
+
+                        richTextBox1.Find(textBox4.Text, indeks_2, richTextBox1.TextLength, RichTextBoxFinds.None);
+                        indeks_2 = richTextBox1.Text.ToLower().IndexOf(textBox4.Text, indeks_2) + 1;
+                        richTextBox1.SelectionBackColor = Color.White;
+                        richTextBox1.SelectedText = " ";
+
+
+                    }
+
+                }
+                else
+                {
+
+                    while (indeks_2 <= richTextBox1.Text.ToLower().LastIndexOf(textBox4.Text))
+                    {
+
+                        richTextBox1.Find(textBox4.Text, indeks_2, richTextBox1.TextLength, RichTextBoxFinds.None);
+                        indeks_2 = richTextBox1.Text.ToLower().IndexOf(textBox4.Text, indeks_2) + 1;
+                        richTextBox1.SelectionBackColor = Color.White;
+                        richTextBox1.SelectedText = " ";
+
+
+                    }
+                }
+
+
+
+
+            }
+        }
+    }
+    }
+
+
+      
+
+
+
